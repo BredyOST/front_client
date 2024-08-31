f9b241f577e94bd716836c134196e2647948b9e4
almakeev <r.makeevv@gmail.com>
2024-08-31 23:39:51 +0300

    - prettier

--- src/app/dashboard/feedback/page.tsx
+++ src/app/dashboard/feedback/page.tsx
@@ -1,38 +1,44 @@
 import React from 'react';
-import cls from './feedback.module.scss'
-import Pictures from "@/app/components/feedbackPage/pictures/pictures";
-
+import cls from './feedback.module.scss';
+import Pictures from '@/app/components/feedbackPage/pictures/pictures';
 
 export const metadata = {
     title: 'Отзывы клиентов - клиенты.com',
     description: 'отзывы',
-}
+};
 
 interface feedBackProps {}
 
 async function getData() {
-
     let feedback;
 
     try {
-        const feedbackRes = await fetch(`${process.env['NEXT_PUBLIC_API_URL']}/files/getAllStart`, { next: { revalidate: 500 } })
+        const feedbackRes = await fetch(
+            `${process.env['NEXT_PUBLIC_API_URL']}/files/getAllStart`,
+            { next: { revalidate: 500 } },
+        );
         if (feedbackRes.ok) {
             const feedbackArray = await feedbackRes.json();
-            if (feedbackArray.length >= 1)     feedback = feedbackArray
+            if (feedbackArray.length >= 1) feedback = feedbackArray;
         } else {
-            console.error('feedbackRes feed API request failed with status:', feedbackRes.status);
+            console.error(
+                'feedbackRes feed API request failed with status:',
+                feedbackRes.status,
+            );
         }
     } catch (err) {
         console.error('save error Redis:', err);
     }
-    return {feedback};
+    return { feedback };
 }
 
-async function Feedback (props:feedBackProps) {
-    const {  } = props;
-    const { feedback} = await getData();
+async function Feedback(props: feedBackProps) {
+    const {} = props;
+    const { feedback } = await getData();
 
-    const nextImages = feedback?.filter((item:any) => /\b2\.\d/.test(item.originalName));
+    const nextImages = feedback?.filter((item: any) =>
+        /\b2\.\d/.test(item.originalName),
+    );
 
     return (
         <div className={cls.feedback}>
@@ -46,6 +52,6 @@ async function Feedback (props:feedBackProps) {
             </div>
         </div>
     );
-};
+}
 
 export default Feedback;
--- src/app/redux/entities/categories/categoriesSlice.ts
+++ src/app/redux/entities/categories/categoriesSlice.ts
@@ -1,24 +1,23 @@
-import {ICategoriesSchema} from "@/app/redux/entities/categories/categoriesSchema";
-import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import { ICategoriesSchema } from '@/app/redux/entities/categories/categoriesSchema';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-
-const initialState:ICategoriesSchema = {
+const initialState: ICategoriesSchema = {
     categories: null,
-    chosenCategory:[],
-}
+    chosenCategory: [],
+};
 
 const categoriesSlice = createSlice({
-    name:'categories',
+    name: 'categories',
     initialState,
     reducers: {
-        addCategories:(state, action:PayloadAction<any>) => {
+        addCategories: (state, action: PayloadAction<any>) => {
             state.categories = action.payload;
         },
-        addChosenCategories:(state, action:PayloadAction<any>) => {
+        addChosenCategories: (state, action: PayloadAction<any>) => {
             state.chosenCategory = action.payload;
-        }
-    }
-})
+        },
+    },
+});
 
 export default categoriesSlice.reducer;
-export const {actions: categoriesActions} = categoriesSlice;
+export const { actions: categoriesActions } = categoriesSlice;
