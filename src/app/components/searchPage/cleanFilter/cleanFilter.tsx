4040e4615482ccc407db6b47ff8c008edf29c99a
almakeev <r.makeevv@gmail.com>
2024-09-01 00:08:41 +0300

    - prettier

--- src/app/components/searchPage/cleanFilter/cleanFilter.tsx
+++ src/app/components/searchPage/cleanFilter/cleanFilter.tsx
@@ -1,28 +1,26 @@
 'use client';
-import React, {FC} from 'react';
-import cls from './cleanFilter.module.scss'
-import {useAppDispatch, useAppSelector} from "@/app/redux/hooks/redux";
-import {SearchParamsActions} from "@/app/redux/entities/searchParams/searchParamsSlice";
+import React, { FC } from 'react';
+import cls from './cleanFilter.module.scss';
+import { useAppDispatch, useAppSelector } from '@/app/redux/hooks/redux';
+import { SearchParamsActions } from '@/app/redux/entities/searchParams/searchParamsSlice';
 
-interface cleanFilterProps {
-}
+interface cleanFilterProps {}
 
-const СleanFilter:FC<cleanFilterProps> = (props) => {
-
+const СleanFilter: FC<cleanFilterProps> = (props) => {
     const dispatch = useAppDispatch();
 
     //ACTIONS FROM REDUX
-    const {keyCityWords} = useAppSelector(state => state.searchParams)
-    const {keyWords} = useAppSelector(state => state.searchParams)
+    const { keyCityWords } = useAppSelector((state) => state.searchParams);
+    const { keyWords } = useAppSelector((state) => state.searchParams);
     //ACTIONS FROM REDUX
-    const {addKeyCityWords} = SearchParamsActions;
-    const {addKeyWords} = SearchParamsActions;
+    const { addKeyCityWords } = SearchParamsActions;
+    const { addKeyWords } = SearchParamsActions;
     //STATES FROM REDUX
-    
+
     //USESTATE
-    
+
     //USEREF
-    
+
     //FUNCTIONS
 
     const cleanFilter = () => {
@@ -33,13 +31,10 @@ const СleanFilter:FC<cleanFilterProps> = (props) => {
             localStorage.setItem('key_words', JSON.stringify([]));
             localStorage.setItem('key_city', JSON.stringify([]));
         }, 0);
-    }
-    
+    };
+
     return (
-        <button
-            className={cls.btn}
-            onClick={cleanFilter}
-        >
+        <button className={cls.btn} onClick={cleanFilter}>
             Сбросить фильтры
         </button>
     );
